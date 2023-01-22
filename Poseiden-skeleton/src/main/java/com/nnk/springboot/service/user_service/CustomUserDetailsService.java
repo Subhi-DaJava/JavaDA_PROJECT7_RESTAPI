703a457aa71799c4b509ca4bce9df9365326719a
Subhi YD <subhy809fr@gmail.com>
2023-01-22 13:04:22 +0100

    4.3.2 add Javadoc for CustomDetailsService class and its method loadUserByUsername

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/user_service/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/user_service/CustomUserDetailsService.java
@@ -11,6 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+/**
+ * The classe implements UserDetailsService,
+ * for retrieving a username(from the database), password and other attributes(ex. authorization info) for authenticating with a username and password.
+ * To load the user information(customer details) during authentication process.
+ * @author Subhi
+ */
 @Service
 @Transactional
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,6 +26,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userService = userService;
     }
 
+    /**
+     * Loading user(with username) from the database and transform the user details(ex.properties, GrantedAuthorities) with SecurityUser.
+     * @param username the username identifying the user whose data is required.
+     * @return UserDetails
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         logger.debug("loadUserByUsername stars here from CustomUserDetailsService");
